84a1effd6271b1a6853e2a9ec507059a39a91412
Stevie Preston <78793772+SteviePreston@users.noreply.github.com>
2024-11-29 21:37:11 +0000

    Update lspconfig.lua

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,1 +1,17 @@
-.
+-- LSPConfig.lua
+
+return {
+  "neovim/nvim-lspconfig",
+  config = function()
+    local lspconfig = require("lspconfig")
+    -- Python
+    lspconfig.pyright.setup({})
+
+    -- Go
+    lspconfig.gopls.setup({})
+
+    -- JavaScript and TypeScript
+    lspconfig.ts_ls.setup({})
+
+  end,
+}
