c254645dd8dbc4f0c702f12998088e6cd3010909
Stevie Preston <78793772+SteviePreston@users.noreply.github.com>
2024-11-29 21:36:23 +0000

    Update cmp.lua

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -1,1 +1,24 @@
-.
+-- CMP.lua
+
+return {
+  "hrsh7th/nvim-cmp",
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",       -- LSP source for nvim-cmp
+    "L3MON4D3/LuaSnip",           -- Snippet engine
+    "saadparwaiz1/cmp_luasnip"    -- Snippet completion source for nvim-cmp
+  },
+  config = function()
+    local cmp = require("cmp")
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          require("luasnip").lsp_expand(args.body)
+        end,
+      },
+      sources = {
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+      },
+    })
+  end,
+}
