a3ab78c9f5b012dc8c1ce0842e41294a76dbda4b
Stevie Preston <78793772+SteviePreston@users.noreply.github.com>
2024-11-29 21:39:21 +0000

    Update themes.lua

--- nvim/lua/plugins/themes.lua
+++ nvim/lua/plugins/themes.lua
@@ -1,1 +1,36 @@
+-- Themes.lua
 
+return {
+    {
+        'ellisonleao/gruvbox.nvim',
+        priority = 1000,
+        config = function()
+            -- Default options:
+            require('gruvbox').setup {
+                terminal_colors = true, -- add Neovim terminal colors
+                undercurl = true,
+                underline = true,
+                bold = true,
+                italic = {
+                    strings = true,
+                    emphasis = true,
+                    comments = true,
+                    operators = false,
+                    folds = true,
+                },
+                strikethrough = true,
+                invert_selection = false,
+                invert_signs = false,
+                invert_tabline = false,
+                invert_intend_guides = false,
+                inverse = true, -- invert search, diffs, statuslines and errors
+                contrast = '', -- can be "hard", "soft" or empty string
+                palette_overrides = {},
+                overrides = {},
+                dim_inactive = false,
+                transparent_mode = false,
+            }
+            vim.cmd 'colorscheme gruvbox'
+        end
+    }
+}
