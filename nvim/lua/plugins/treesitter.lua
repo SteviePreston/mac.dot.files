aa7be436c4f73522f35ffb21a68a85b91493b469
Stevie Preston <78793772+SteviePreston@users.noreply.github.com>
2024-11-29 21:40:14 +0000

    Update treesitter.lua

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,1 +1,12 @@
-.
+-- Treesitter.lua
+
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = {"go", "python", "lua", "javascript", "typescript"}, 
+      highlight = { enable = true },
+    })
+  end,
+}
